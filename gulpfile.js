fd38521f584c64ba9966374777f1acfbeedf5b26
Justin Furniss <primeobsession@gmail.com>
2016-07-25 23:35:20 -0400

    Some code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,42 @@
 var gulp   = require( 'gulp' ),
-  gulpLoadPlugins = require('gulp-load-plugins'),
+
   server = require( 'gulp-develop-server'),
-  config = require('./config');
+  config = require('./config'),
+  livereload = require('gulp-livereload'),
+  //connect = require('gulp-connect'),
+  gulpLoadPlugins = require('gulp-load-plugins');
 
 var plugins = gulpLoadPlugins();
 
 gulp.task( 'server:develop', function() {
+
   server.listen( { path: './server.js' } );
 });
 
 gulp.task('default', [ 'server:develop', 'server:restart']);
 
+gulp.task('reloadserver', function(){
+  return server.restart(function(){
+    plugins.livereload.reload();
+  });
+});
+
+gulp.task('reset', function(){
+  //server.restart();
+
+
+
+  //return gulp.src('*')
+  //  .pipe(plugins.livereload.changed)
+
+  //livereload();
+});
+
 gulp.task( 'server:restart', function() {
-  gulp.watch( [ './*.js', './*.html', './config.json', config.bundle + '/**'], server.restart );
+
+  gulp.watch( [ './*.js', './*.html', './*.css', './config.json', config.bundle + '/**'], [ 'reloadserver']).on('change', function(){
+    console.log('weeee~!');
+
+  });
+  plugins.livereload.listen();
 });
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var express = require('express'),
   request = require('request'),
   config = require('./config'),
   _ = require('lodash'),
-  bundleConfig = config + '/app.json',
   app = express();
 
 
@@ -22,37 +21,74 @@ console.log('', '-', 'Loaded css');
 var jsSrc = fs.readFileSync(config.bundle + '/' + bundleConf.bundles[0].js, 'utf8');
 console.log('', '-', 'Loaded js');
 
+// Get the mount point specified in the package bundle for the images
 var mountpoint = '/app/' + bundleConf.images;
+
+// Deal with trailing slash if it exists
 if(mountpoint[mountpoint.length-1] === '/'){
   mountpoint = mountpoint.substr(0, mountpoint.length-1);
 }
-console.log('', '-', 'mounting images ' + mountpoint + ' to ', config.bundle + '/' + bundleConf.images);
 
+console.log('', '-', 'mounting images ' + mountpoint + ' to ', config.bundle + '/' + bundleConf.images);
 app.use(mountpoint, express.static(config.bundle + '/' + bundleConf.images));
 
+
+// Execute polling requests specified in the bundle pacakge
 var loadPollingData = function(cb){
+
+  // Loop through each request
   async.each(bundleConf.requests, function(configRequest, rcb){
 
     var url = configRequest.url;
-    _.each(config.params.config, function(p){
-      url = url.replace('{' + p.name + '}', p.value);
+
+    _.each(config.params, function(p){
+
+      var val = p.value;
+      var vals = [];
+
+      // If the param value looks like JSON, attempt to parse it into a proper JS obj
+      if (val.indexOf('{')===0){
+
+        try {
+          var json = JSON.parse(p.value);
+          for (var prop in json) {
+            vals.push({
+              name: p.name + '.' + prop,
+              value: json[prop]
+            });
+          }
+
+        } catch (e) {
+          vals.push(p);
+        }
+      }
+      else{
+        vals.push(p);
+      }
+
+
+      _.each(vals, function(v){
+        url = url.replace('{' + v.name + '}', v.value);
+      });
+
     });
 
     console.log('_', 'making request', url);
+    // Make the HTTP request and pipe the output to a local JSON file so we don't need to make the request over and
+    // over again while debugging
     request({
       url: url,
       headers: configRequest.headers
     }, function(err, res, body){
-
       rcb(err);
     }).on('response', function(response) {
-        //console.log(response.statusCode) // 200
         console.log('_', response.statusCode, response.headers['content-type']); // 'image/png'
       })
       .pipe(fs.createWriteStream('./cache/' + configRequest.name + '.json'));
   }, cb);
 };
 
+// Grab the cached request data
 var loadCachedData = function(cb){
   var data = {};
   async.each(bundleConf.requests, function(configRequest, rcb){
@@ -65,6 +101,7 @@ var loadCachedData = function(cb){
   });
 };
 
+// Iterate through the specified includes and return them in a callback
 var loadIncludes = function(cb){
   var includes = [];
 
@@ -83,9 +120,10 @@ var loadIncludes = function(cb){
   });
 };
 
-
+// Compile the swig template
 var tpl = swig.compileFile('./view.html');
 
+// GET /refresh - Forces polling data to be refreshed
 app.get('/refresh', function(req, res){
   loadPollingData(function(err){
     if(err){
@@ -97,12 +135,15 @@ app.get('/refresh', function(req, res){
   });
 });
 
+// GET /app - redirects to app index
 app.get('/app', function(req, res){
   res.redirect('/app/index.html');
 });
 
+// GET /app/index.html - Returns compiled HTML similar to what will be returned from the Props app server.
 app.get('/app/index.html', function(req, res){
 
+  // Grab the information needed to compile HTML for the app
   async.waterfall([
     //Load cached data
     function(cb){
@@ -116,9 +157,15 @@ app.get('/app/index.html', function(req, res){
     }
   ], function(err, data, incs){
 
+    // Inject app information into variable that will be compiled into the app
     var appData = config.app;
     appData.config = config.params;
+    appData.configValues = {};
+    _.each(config.params, function(p){
+      appData.configValues[p.name] = p.value;
+    });
 
+    // Provide the gathered information into the template, render it, then send it back to the client.
     res.send(tpl({
       data: data,
       app: appData,
